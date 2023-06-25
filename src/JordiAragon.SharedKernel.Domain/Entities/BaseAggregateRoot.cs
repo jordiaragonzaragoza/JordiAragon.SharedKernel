75eca6553f659e1975c766f8a6e95101ae68084b
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2023-06-25 21:27:52 +0200

    Crash: ticketBuilder.WithOwner().HasForeignKey(nameof(ShowtimeId));

--- src/JordiAragon.SharedKernel.Domain/Entities/BaseAggregateRoot.cs
+++ src/JordiAragon.SharedKernel.Domain/Entities/BaseAggregateRoot.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations.Schema;
+    using Ardalis.GuardClauses;
     using JordiAragon.SharedKernel.Domain.Contracts.Interfaces;
 
     public abstract class BaseAggregateRoot<TId, TIdType> : BaseEntity<TId>, IAggregateRoot<TId>
@@ -12,10 +13,12 @@
         protected BaseAggregateRoot(TId id)
             : base(id)
         {
+            this.Id = Guard.Against.Null(id, nameof(id));
         }
 
         // Required by EF
         protected BaseAggregateRoot()
+            : base()
         {
         }
 
--- src/JordiAragon.SharedKernel.Domain/Entities/BaseAuditableEntity.cs
+++ src/JordiAragon.SharedKernel.Domain/Entities/BaseAuditableEntity.cs
@@ -11,6 +11,7 @@
         }
 
         protected BaseAuditableEntity()
+            : base()
         {
         }
 
