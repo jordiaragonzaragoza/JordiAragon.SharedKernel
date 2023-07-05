4c352b8a6d4656637eaf0c5f6bdab7a7f2bd0276
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2023-07-05 11:50:42 +0200

    Added DomainEventsDispatcherNotificationHandlerDecorator.

--- src/JordiAragon.SharedKernel.Application/AssemblyConfiguration/ApplicationModule.cs
+++ src/JordiAragon.SharedKernel.Application/AssemblyConfiguration/ApplicationModule.cs
@@ -1,10 +1,22 @@
 ﻿namespace JordiAragon.SharedKernel.Application.AssemblyConfiguration
 {
     using System.Reflection;
+    using Autofac;
     using JordiAragon.SharedKernel;
+    using JordiAragon.SharedKernel.Application.Events.Decorators;
+    using MediatR;
 
     public class ApplicationModule : AssemblyModule
     {
         protected override Assembly CurrentAssembly => ApplicationAssemblyReference.Assembly;
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterGenericDecorator(
+                typeof(DomainEventsDispatcherNotificationHandlerDecorator<>),
+                typeof(INotificationHandler<>));
+        }
     }
 }
---
+++ src/JordiAragon.SharedKernel.Application/Events/Decorators/DomainEventsDispatcherNotificationHandlerDecorator.cs
@@ -0,0 +1,29 @@
+﻿namespace JordiAragon.SharedKernel.Application.Events.Decorators
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using JordiAragon.SharedKernel.Application.Contracts.Interfaces;
+    using JordiAragon.SharedKernel.Domain.Contracts.Interfaces;
+
+    public class DomainEventsDispatcherNotificationHandlerDecorator<T> : IDomainEventHandler<T>
+        where T : IDomainEvent
+    {
+        private readonly IDomainEventHandler<T> decorated;
+        private readonly IDomainEventsDispatcher domainEventsDispatcher;
+
+        public DomainEventsDispatcherNotificationHandlerDecorator(
+            IDomainEventsDispatcher domainEventsDispatcher,
+            IDomainEventHandler<T> decorated)
+        {
+            this.domainEventsDispatcher = domainEventsDispatcher;
+            this.decorated = decorated;
+        }
+
+        public async Task Handle(T notification, CancellationToken cancellationToken)
+        {
+            await this.decorated.Handle(notification, cancellationToken);
+
+            await this.domainEventsDispatcher.DispatchDomainEventsAsync(cancellationToken);
+        }
+    }
+}
