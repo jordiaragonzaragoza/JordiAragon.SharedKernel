189be8d674831050c5a518f4c9b3039ee38db587
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2024-07-07 20:24:22 +0200

    Added IEventHandler and IEventNotificationHandler.

---
+++ src/JordiAragon.SharedKernel.Application.Contracts/Interfaces/IEventNotificationHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace JordiAragon.SharedKernel.Application.Contracts.Interfaces
+{
+    using JordiAragon.SharedKernel.Contracts.Events;
+    using MediatR;
+
+    public interface IEventNotificationHandler<in TEventNotification> : INotificationHandler<TEventNotification>
+        where TEventNotification : IEventNotification
+    {
+    }
+}
