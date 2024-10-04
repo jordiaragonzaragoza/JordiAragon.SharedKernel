a7b7f706124f541458dcb1b8f2feb09faf3f3e3a
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2024-10-04 17:47:48 +0200

    Fix LoggerFactory. Dispose is not required.

--- src/JordiAragon.SharedKernel.Infrastructure.EntityFramework/Context/BaseContext.cs
+++ src/JordiAragon.SharedKernel.Infrastructure.EntityFramework/Context/BaseContext.cs
@@ -1,6 +1,5 @@
 ﻿namespace JordiAragon.SharedKernel.Infrastructure.EntityFramework.Context
 {
-    using System;
     using Ardalis.GuardClauses;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Hosting;
@@ -9,9 +8,9 @@
 
     public abstract class BaseContext : DbContext
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "It is ok for ILoggerFactory is managed by dependency inyection.")]
         private readonly ILoggerFactory loggerFactory;
         private readonly IHostEnvironment hostEnvironment;
-        private bool disposed;
 
         protected BaseContext(
             DbContextOptions options,
@@ -21,22 +20,6 @@
         {
             this.loggerFactory = Guard.Against.Null(loggerFactory, nameof(loggerFactory));
             this.hostEnvironment = Guard.Against.Null(hostEnvironment, nameof(hostEnvironment));
-        }
-
-        public override void Dispose()
-        {
-            if (!this.disposed)
-            {
-                this.loggerFactory?.Dispose();
-
-                this.disposed = true;
-
-#pragma warning disable S3971 // "GC.SuppressFinalize" should not be called
-                GC.SuppressFinalize(this);
-#pragma warning restore S3971 // "GC.SuppressFinalize" should not be called
-            }
-
-            base.Dispose();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
